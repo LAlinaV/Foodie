92b5e5aa8891f885c1af35f4610dbebaf178e076
Vasya-Sheynin <vasya.sheynin2005@gmail.com>
2024-05-24 10:55:15 -0700

    Login/Register added.

--- Foodie/ContentView.swift
+++ Foodie/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    
-    @State var changeScreen = false
+    @State private var showLoginScreen = false
+    @State private var showRegisterScreen = false
+
     var body: some View {
         NavigationView {
             VStack(spacing: 30) {
@@ -24,47 +25,45 @@ struct ContentView: View {
                 Text("Grocerr gives you fresh vegetables and fruits,\nOrder fresh at grocerr")
                     .multilineTextAlignment(.center)
                     .foregroundColor(.gray)
-                Button() {
-                    changeScreen = true
-                } label: {
+                Button(action: {
+                    self.showLoginScreen = true
+                }) {
                     ZStack {
                         RoundedRectangle(cornerRadius: 50)
+                            .foregroundColor(.green)
                         Text("Login")
                             .foregroundColor(.white)
                             .bold()
                     }
-                }.frame(width: 200, height: 35)
-                    .foregroundColor(.green)
-            
-            .navigationViewStyle(StackNavigationViewStyle())
-            .fullScreenCover(isPresented: $changeScreen) {
-                Shop()
-            }
-                Button() {
-                    changeScreen = true
-                } label: {
+                }
+                .frame(width: 200, height: 35)
+                .fullScreenCover(isPresented: $showLoginScreen) {
+                    Login()
+                }
+
+                Button(action: {
+                    self.showRegisterScreen = true
+                }) {
                     ZStack {
                         RoundedRectangle(cornerRadius: 50)
+                            .foregroundColor(.green)
                         Text("Register")
                             .foregroundColor(.white)
                             .bold()
                     }
-                }.frame(width: 200, height: 35)
-                    .foregroundColor(.green)
-            
-            .navigationViewStyle(StackNavigationViewStyle())
-            .fullScreenCover(isPresented: $changeScreen) {
-                Registration()
+                }
+                .frame(width: 200, height: 35)
+                .fullScreenCover(isPresented: $showRegisterScreen) {
+                    Registration()
+                }
             }
-                
+            .navigationViewStyle(StackNavigationViewStyle())
         }
-     }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
-}
-
 }
--- Foodie/Shop.swift
+++ Foodie/Shop.swift
@@ -23,47 +23,51 @@ struct Shop: View {
     var items: [[Any]] = shopItems
     
     var body: some View {
-        NavigationView {
-        
-            
-            /*SearchView(searchText: $searchText)
-                .frame(height:50)
-                .padding(.horizontal)
-            */
-            ScrollView() {
-                LazyVGrid(columns: columns, spacing: 30) {
-                    ForEach(0..<items.count, id:\.self) { item in
-                        ShopItem(imageName: items[item][0] as! String, title: items[item][1] as! String, price: items[item][2] as! Double, color: items[item][3] as! Color, selfIndex: item)
+        if #available(iOS 15.0, *) {
+            NavigationView {
+                
+                
+                /*SearchView(searchText: $searchText)
+                 .frame(height:50)
+                 .padding(.horizontal)
+                 */
+                ScrollView() {
+                    LazyVGrid(columns: columns, spacing: 30) {
+                        ForEach(0..<items.count, id:\.self) { item in
+                            ShopItem(imageName: items[item][0] as! String, title: items[item][1] as! String, price: items[item][2] as! Double, color: items[item][3] as! Color, selfIndex: item)
+                        }
                     }
-                }
-            }.padding(.bottom, 15)
-        
-        
-        .fullScreenCover(isPresented: $goToCart) {
-            Cart()
-        }
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }) {
-                    Image(systemName: "chevron.left")
-                    Text("Back")
-                }
+                }.padding(.bottom, 15)
+                
+                
+                    .fullScreenCover(isPresented: $goToCart) {
+                        Cart()
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            Button(action: {
+                                self.presentationMode.wrappedValue.dismiss()
+                            }) {
+                                Image(systemName: "chevron.left")
+                                Text("Back")
+                            }
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button() {
+                                goToCart = true
+                            }label: {
+                                Image(systemName: "cart")
+                            }
+                        }
+                    }
+                
             }
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button() {
-                    goToCart = true
-                }label: {
-                    Image(systemName: "cart")
-                }
-            }
+            .navigationViewStyle(.stack)
+            .searchable(text: $searchText, prompt: "Food and products")
+            .onChange(of: searchText) { newValue in filterItems(searchText: newValue)}
+        } else {
+            // Fallback on earlier versions
         }
-        
-    }
-        .navigationViewStyle(.stack)
-        .searchable(text: $searchText, prompt: "Food and products")
-        .onChange(of: searchText) { newValue in filterItems(searchText: newValue)}
         
     }
     func filterItems(searchText: String){
